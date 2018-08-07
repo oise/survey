d5268c939f9b9f120b36a55edde34c4e99a7e280
oise <hello@oise.me>
2018-08-07 19:14:42 +0100

    sample data file

---
+++ src/sampleData/data.ts
@@ -0,0 +1,107 @@
+export const data = [
+    {
+        "question": "Pariatur laborum velit minim irure adipisicing consectetur velit quis ut incididunt minim mollit aliqua. Cupidatat aute Lorem exercitation sunt dolore sunt eu enim ullamco ipsum est et duis. Adipisicing ea irure ipsum exercitation et esse nisi dolor. Velit sit deserunt deserunt veniam consequat enim ipsum elit est consequat ut ea pariatur aliquip. Deserunt eiusmod tempor non sunt reprehenderit veniam qui sint in nulla. Excepteur enim incididunt nostrud do consectetur mollit sit qui aliqua magna ipsum ullamco. Veniam officia excepteur dolor aliquip voluptate consequat nulla id commodo Lorem.",
+        "optionType": "checkbox",
+        "options": [
+            {
+                "option": "minim"
+            },
+            {
+                "option": "qui"
+            },
+            {
+                "option": "occaecat"
+            }
+        ]
+    },
+    {
+        "question": "Sunt aute cillum et qui non Lorem. Velit excepteur reprehenderit et elit officia adipisicing. Eu eu pariatur dolore ullamco et enim ea et exercitation. Ad velit quis in Lorem est. Ex pariatur ad enim quis occaecat officia eu excepteur proident commodo aliqua ipsum veniam incididunt. Id duis anim elit minim duis deserunt.",
+        "optionType": "checkbox",
+        "options": [
+            {
+                "option": "reprehenderit"
+            },
+            {
+                "option": "quis"
+            },
+            {
+                "option": "fugiat"
+            }
+        ]
+    },
+    {
+        "question": "Eiusmod eu laborum ullamco voluptate commodo duis elit elit adipisicing irure culpa culpa. Nulla dolore duis nisi ipsum amet do fugiat nulla exercitation. Occaecat eu dolor fugiat cillum voluptate elit esse tempor consequat ex esse nisi magna occaecat.",
+        "optionType": "plain",
+        "options": []
+    },
+    {
+        "question": "Est veniam laboris incididunt aliqua excepteur fugiat velit dolor dolore. Laborum sit minim adipisicing sit consectetur proident anim non. Nisi esse reprehenderit voluptate elit commodo. Cupidatat nulla nostrud esse officia. Incididunt ut eu magna duis elit voluptate in duis sit et exercitation laboris officia. Veniam Lorem ipsum culpa enim eiusmod nisi ea aliqua sit enim. Magna reprehenderit occaecat excepteur fugiat adipisicing enim commodo.",
+        "optionType": "plain",
+        "options": []
+    },
+    {
+        "question": "Laborum adipisicing ut non pariatur aliqua. Adipisicing aliqua dolor exercitation tempor voluptate aliquip. Irure irure elit labore eiusmod fugiat id nulla velit eu laboris elit nulla velit. Est dolore adipisicing officia excepteur dolore. Excepteur reprehenderit magna anim ex labore ad magna reprehenderit aliquip officia.",
+        "optionType": "plain",
+        "options": []
+    },
+    {
+        "question": "Laboris nostrud sit commodo esse ut nulla qui aliquip labore mollit irure excepteur labore. Do labore fugiat non ex labore fugiat nostrud deserunt in Lorem qui nisi. Anim aliquip ipsum est est ex. Est deserunt commodo in tempor duis fugiat id nostrud aute exercitation cupidatat ipsum. Enim sunt ad ut ullamco eu aliqua deserunt deserunt. Proident ex non ea aute consequat consequat voluptate aute exercitation officia nostrud sit minim consectetur. Aliquip in sit ut deserunt elit fugiat enim labore veniam tempor consectetur.",
+        "optionType": "dropdown",
+        "options": [
+            {
+                "option": "ullamco"
+            },
+            {
+                "option": "veniam"
+            },
+            {
+                "option": "sit"
+            }
+        ]
+    },
+    {
+        "question": "Qui adipisicing ut sit nisi consectetur cupidatat aliqua ea nostrud culpa ex. Anim incididunt eu nulla non excepteur ea irure duis. Ad ipsum cupidatat commodo veniam fugiat ut laboris sunt incididunt. Ex nisi qui excepteur dolor dolore tempor elit. Et nostrud nulla in magna esse. Elit cillum adipisicing cupidatat enim anim.",
+        "optionType": "dropdown",
+        "options": [
+            {
+                "option": "reprehenderit"
+            },
+            {
+                "option": "sint"
+            },
+            {
+                "option": "sit"
+            }
+        ]
+    },
+    {
+        "question": "Sunt deserunt cupidatat eu cupidatat laborum duis occaecat esse et. Cupidatat pariatur sunt sint mollit sint. Ex fugiat dolore adipisicing deserunt ipsum ut voluptate duis anim enim quis commodo do aliqua. Id commodo enim sint minim exercitation eu eu deserunt ad reprehenderit minim Lorem.",
+        "optionType": "checkbox",
+        "options": [
+            {
+                "option": "qui"
+            },
+            {
+                "option": "duis"
+            },
+            {
+                "option": "anim"
+            }
+        ]
+    },
+    {
+        "question": "Enim id pariatur in magna. Cupidatat duis enim ullamco enim consequat labore et. Quis ex reprehenderit irure velit incididunt adipisicing aliquip.",
+        "optionType": "checkbox",
+        "options": [
+            {
+                "option": "id"
+            },
+            {
+                "option": "commodo"
+            },
+            {
+                "option": "culpa"
+            }
+        ]
+    }
+]
